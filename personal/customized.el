edff68eca85846541fbbf7be412eb2deb436f841
Wudong Liu <wudong.liu@gmail.com>
2016-12-13 21:06:21 +0000

    require magit, and neotree package.

--- personal/customized.el
+++ personal/customized.el
@@ -14,6 +14,43 @@
 (prelude-require-package 'goto-last-change)
 (global-set-key (kbd "C-<") 'goto-last-change)
 
+;; required for neotree's icon theme;
+;; also need to download and install the fonts.
+(prelude-require-package 'all-the-icons)
+
+;; magit
+(prelude-require-package 'magit)
+
+;; neotree
+(prelude-require-package 'neotree)
+
+;; neotree configurations
+
+;; every time when the neotree window is opened, let it find
+;; current file and jump to node.
+(setq neo-smart-open t)
+
+;; set the theme to use icon while in graphical and arrow
+;; while in console
+(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+
+;; change root automatically when running projectile-switch-project
+(setq projectile-switch-project-action 'neotree-projectile-action)
+
+;; open neotree at projectile project root
+(defun neotree-project-dir ()
+    "Open NeoTree using the git root."
+    (interactive)
+    (let ((project-dir (projectile-project-root))
+          (file-name (buffer-file-name)))
+      (if project-dir
+          (if (neotree-toggle)
+              (progn
+                (neotree-dir project-dir)
+                (neotree-find file-name)))
+        (message "Could not find git project root."))))
+(global-set-key [f8] 'neotree-project-dir)
+
 ;; auto-fill mode to automatic wrap line in text mode.
 (add-hook 'text-mode-hook 'turn-on-auto-fill)
 
