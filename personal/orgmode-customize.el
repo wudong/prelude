47f631186a1d0f492d05824e5ec7418ae9e5a121
Wudong Liu <wudong.liu@gmail.com>
2019-01-25 12:56:49 +0000

    local changes

--- personal/orgmode-customize.el
+++ personal/orgmode-customize.el
@@ -26,7 +26,7 @@
               )))
 
 ;; syntax hightlight in orgmode
-;; http://stackoverflow.com/questions/10642888/syntax-highlighting-within-begin-src-block-in-emacs-orgmode-not-working
+;;  http://stackoverflow.com/questions/10642888/syntax-highlighting-within-begin-src-block-in-emacs-orgmode-not-working
 (setq org-src-fontify-natively t)
 ;; set agenda files
 (setq org-agenda-files (quote (concat org-directory "/gtd.org")))
@@ -61,28 +61,3 @@
         ("s" "Scribble" entry (file (concat org-directory "/scribble.org"))
          "* %?\nEntered on %U\n  %i\n")
         ))
-
-;; ;; setting up sync with google calendar.
-;; ;; (prelude-require-package 'org-gcal)
-;; ;; (require'org-gcal)
-
-;; ;; (setq org-gcal-client-id "447777984749-qn6b8d232jheo97t0tneqknojqgdiam5.apps.googleusercontent.com"
-;; ;;       org-gcal-client-secret "VLMI7ebbYxPyZG1zWP_aQCuO"
-;; ;;       org-gcal-file-alist '(("wudong.liu@gmail.com" .  "~/Repository/org/schedule.org")
-;; ;; ))
-;; ;; call the gcal sync when necessary.
-;; ;; (add-hook 'org-agenda-mode-hook (lambda () (org-gcal-sync) ))
-;; ;; (add-hook 'org-capture-after-finalize-hook (lambda () (org-gcal-sync) ))
-
-;; ;; blogging with orgmode, publishing to wordpress.
-;; (setq org2blog/wp-blog-alist
-;;       '(("graceliu"
-;;          :url "https://www.graceliu.uk/xmlrpc.php"
-;;          :username "wudong"
-;;          :password "yuepan2008"
-;;          :wp-code t
-;;          :wp-latex t
-;;          :default-title "title"
-;;          )))
-
-;; (setq org2blog/wp-default-categories '("编程") )
